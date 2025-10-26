b64abe856c23ac69596d23ba0c59ba0c109cb850
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2025-10-26 18:35:15 +0700

    database

--- init.sql
+++ init.sql
@@ -8,3 +8,59 @@ CREATE TABLE IF NOT EXISTS users (
     address VARCHAR(255) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE
 );
+
+ALTER TABLE users
+ADD phoneNum VARCHAR(15),
+ADD password VARCHAR(50) NOT NULL;
+
+CREATE TABLE IF NOT EXISTS saler (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    phoneNum VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orderHeader (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    salerId INT,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    totalPrice INT NOT NULL, 
+    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (salerId) REFERENCES saler(id) 
+);
+
+CREATE TABLE IF NOT EXISTS category(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS product (
+    id  INT AUTO_INCREMENT PRIMARY KEY,
+    categoryId INT,
+    name VARCHAR(50) NOT NULL,
+    price INT NOT NULL,
+    size VARCHAR(10),
+    FOREIGN KEY (categoryId) REFERENCES category(id)
+);
+
+CREATE TABLE IF NOT EXISTS orderDetail (
+    orderId INT ,
+    productId INT,
+    quantity INT NOT NULL,
+    FOREIGN KEY (orderId) REFERENCES orderHeader(id),
+    FOREIGN KEY (productId) REFERENCES product(id)
+);
+
+CREATE TABLE IF NOT EXISTS review(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    productId INT,
+    rate FLOAT NOT NULL,
+    review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description VARCHAR(100),
+    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (productId) REFERENCES product(id),
+);
